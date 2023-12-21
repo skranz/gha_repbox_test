9a5d32079c0e7e17b6ec4ab6f2fedd08872d0615
Sebastian Kranz <sebkranz@gmail.com>
2023-12-21 19:09:42 +0100

    Updates

--- scripts/install.R
+++ scripts/install.R
@@ -1,6 +1,6 @@
 # Here we can add some custom package installations
 
 library(remotes)
-remotes::install_github("repboxr/sourcemodify", upgrade="never", force=FALSE)
+#remotes::install_github("repboxr/sourcemodify", upgrade="never", force=FALSE)
 remotes::install_github("repboxr/repboxMap", upgrade="never", force=FALSE)
 remotes::install_github("repboxr/repboxHtml", upgrade="never", force=FALSE)
