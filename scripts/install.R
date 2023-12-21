ab7aa705a8d2c62482499ce891f65345a16f9147
Sebastian Kranz <sebkranz@gmail.com>
2023-12-21 17:20:10 +0100

    Update install.R

--- scripts/install.R
+++ scripts/install.R
@@ -13,7 +13,8 @@ download_repbox_pkg_zip_from_github = function(dest_dir, user="repboxr", pkgs,  
 unzip_pkgs_sources = function(zip_dir, dest_dir=zip_dir) {
   files = list.files(zip_dir, glob2rx("*.zip"),ignore.case = TRUE,full.names = TRUE)
   for (file in files) {
-    unzip(file,exdir = zip_dir)
+    cat("\nUnzip ", file, " to ", dest_dir)
+    unzip(file,exdir = dest_dir)
   }
 }
 
@@ -33,5 +34,8 @@ cat("\n\nDownload repbox packages as ZIP from Github...\n\n")
 download_repbox_pkg_zip_from_github(pkg_dir, user="repboxr", pkgs = pkgs)
 cat("\n\nExtract packages ZIP...\n\n")
 unzip_pkgs_sources(pkg_dir)
+
+cat("\n\nDirectories in ", pkg_dir,"\n\n")
+list.dirs(pkg_dir,recursive = FALSE)
 cat("\n\nBuild and install packages...\n\n")
 install_pkg_sources(pkg_dir, pkgs)
